1cd2bcbf6514f6ad1a7d3ae619f398ea41ce8437
Gabriel Lemos <117481990+lemosg189@users.noreply.github.com>
2023-07-17 20:38:50 -0300

    sem comentários

--- src/rotas.js
+++ src/rotas.js
@@ -8,10 +8,10 @@ const {
 } = require("./controllers/usuarios");
 const rotas = express();
 
-rotas.get("/usuarios", listarUsuarios); //obter
-rotas.get("/usuarios/:id", listarUmUsuario); //obter
-rotas.post("/usuarios", cadastrarUsuario); //criar
-rotas.put("/usuarios/:id", atualizarUsuario); // atualizar
+rotas.get("/usuarios", listarUsuarios); 
+rotas.get("/usuarios/:id", listarUmUsuario); 
+rotas.post("/usuarios", cadastrarUsuario); 
+rotas.put("/usuarios/:id", atualizarUsuario); 
 rotas.delete("/usuarios/:id", deletarUsuario);
 
 module.exports = rotas;
